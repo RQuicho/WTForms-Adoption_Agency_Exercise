66da85f56d02af870d5f3f00636f25b2116c8f08
RQuicho <raymondquicho@gmail.com>
2023-05-28 13:41:49 -0700

    creating database and sample data in seed.py

--- app.py
+++ app.py
@@ -5,9 +5,11 @@ from models import db, connect_db
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///adoptions_db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 app.config['SECRET_KEY'] = "#adoptapet^now"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 debug = DebugToolbarExtension(app)
+
+connect_db(app)
---
+++ seed.py
@@ -0,0 +1,18 @@
+from models import db, Pet
+from app import app
+
+# Create all tables
+db.drop_all()
+db.create_all()
+
+# Clear any existing data in tables
+Pet.query.delete()
+
+# Add sample data
+p1 = Pet(name='Eeyore', species='donkey', photo_url='https://wallpapercave.com/wp/womqvTH.jpg', age=5, notes='pessimistic, gloomy, and depressed')
+p2 = Pet(name='Piglet', species='pig', photo_url='https://womenwriteaboutcomics.com/wp-content/uploads/2017/01/Piglet.jpg', age=2, notes='timid, brave, friendly', available=0)
+p3 = Pet(name='Kanga', species='kangaroo', photo_url='https://clipartspub.com/images/pooh-clipart-kanga-8.jpg', age=10, notes='hospitable, gentle', available=1)
+
+db.session.add_all([p1, p2, p3])
+db.session.commit()
+
