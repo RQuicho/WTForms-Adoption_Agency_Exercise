612ec0932a8c6c660966849c4d5dcbf2e7c1b45f
RQuicho <raymondquicho@gmail.com>
2023-05-28 13:09:50 -0700

    adding set up for project

---
+++ forms.py
@@ -0,0 +1,3 @@
+from flask_wtf import FlaskForm
+from wtforms import Stringfield, FloatField, BooleanField, IntegerField, RadioField, SelectField
+from wtforms.validators import InputRequired, Email
