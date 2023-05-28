7186c500787a08f87b855781bf515543a864320a
RQuicho <raymondquicho@gmail.com>
2023-05-28 14:14:12 -0700

    adding route that shows all pets

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, render_template, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
-from models import db, connect_db
+from models import db, connect_db, Pet
 
 
 app = Flask(__name__)
@@ -13,3 +13,11 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 debug = DebugToolbarExtension(app)
 
 connect_db(app)
+
+@app.route('/')
+def list_all_pets():
+    """Show list of all pets"""
+
+    pets = Pet.query.all()
+
+    return render_template('pets_list.html', pets=pets)
